338da75b664c6c882406a93c089fc9c63e26ec96
ShahRiarKhan55 <siaabid1234@gmail.com>
2023-12-07 21:34:23 +0600

    2nd Commit

--- server/index.js
+++ server/index.js
@@ -3,27 +3,30 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-const { Console } = require("console");
+
 app.use(cors());
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
 
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-io.on("connection",  (socket) => {
-  console.log(socket.id);
-
-  socket.on("disconnect", ()=>{
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+  socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
-    
   });
 });
 
 server.listen(3001, () => {
-  console.log("Sever Started");
+  console.log("Server Started");
 });
